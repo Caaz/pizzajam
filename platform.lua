5a9652d8cf0d2fa00b011e81e690c94273ab8d32
Daniel Caaz <daniel@caaz.me>
2018-02-10 17:39:39 -0800

    Add arrows

--- platform.lua
+++ platform.lua
@@ -10,8 +10,8 @@ class_platform = class{
     this:set_side(this.set or rnd()>.5)
   end,
   set_side = function(this,v)
-    local antikey = (v and 'y' or 'x')
     local key = (v and 'x' or 'y')
+    local antikey = (v and 'y' or 'x')
     local positive = rnd()>.5
     this['vel_'..key] = (positive and 2 or -2)
     this[key] = (positive and -64 or 192)
@@ -34,5 +34,21 @@ class_platform = class{
   draw = function(this)
     local x,y = flr(this.x),flr(this.y)
     rect(x, y, x + this.width-1, y + this.height-1, (this.clear and 5 or 7))
+
+    local sprite
+    if this.vel_y > 0 then
+      -- down
+      sprite = 19
+    elseif this.vel_y < 0 then
+      -- up
+      sprite = 18
+    elseif this.vel_x > 0 then
+      -- right
+      sprite = 17
+    else
+      -- left
+      sprite = 16
+    end
+    spr(sprite,min(max(this.x,0),120),min(max(this.y,0),120))
   end
 }
